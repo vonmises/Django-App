eb9bf067a4a138ffca094e155015ff1049aa727e
vonmises <vonmises@users.noreply.github.com>
2015-10-06 10:20:50 +0300

    added form logic to allow saving

--- firstproject/home/views.py
+++ firstproject/home/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from .forms import StudentForm
 
 # Create your views here.
 
 def index(request):
+    form = StudentForm(request.POST or None)
+
     context = {
-              "hello_message": "Hello from Moringa"
+              "hello_message": "Hello from Moringa",
+              "form": form
     }
+
+    if form.is_valid():
+        form.save()
+        context = {
+                  "hello_message": "Student saved",
+        }
+
     return render(request, 'index.html', context)
