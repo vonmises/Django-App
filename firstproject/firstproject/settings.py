7a6714886c8c0f758a48e76ac6d42c499e459424
vonmises <vonmises@users.noreply.github.com>
2015-10-09 19:57:06 +0300

    added static_root and staticfiles_dirs variables

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -106,3 +106,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'assets', 'static_production')
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'assets', 'static'),
+)
