8fb2afaef192be0de779ca1b82ac660dc8045760
vonmises <vonmises@users.noreply.github.com>
2015-10-13 11:32:59 +0300

    added students view with filter

--- firstproject/home/views.py
+++ firstproject/home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.core.mail import send_mail
 
 from .forms import StudentForm, FeedbackForm
+from .models import Student
 
 # Create your views here.
 
@@ -35,13 +36,23 @@ def feedback(request):
         last_name = form.cleaned_data.get('last_name')
         full_name = first_name + ' ' + last_name
         message = form.cleaned_data.get('message')
-        formatted_message = 'You have feedback from {} saying "{}".'.format(full_name, message)
+        formatted_message = 'You have feedback from {} saying "{}".' \
+                            .format(full_name, message)
         from_email = form.cleaned_data.get('email')
         send_mail('Feedback', formatted_message, from_email,
                  ['email'], fail_silently=False)
 
     context = {
-              "form": form
+              'form': form
     }
 
     return render(request, 'feedback.html', context)
+
+def students(request):
+    search_term = request.GET['search']
+    students = Student.objects.all().order_by('-last_update') \
+                      .filter(last_name__contains=search_term)
+    context = {
+              'students': students
+    }
+    return render(request, 'students.html', context)
