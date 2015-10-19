924d6a1467f157705078ffa94a1e9701217d481c
vonmises <vonmises@users.noreply.github.com>
2015-10-19 19:07:54 +0300

    addded path for bootstrap templates

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -64,7 +64,8 @@ ROOT_URLCONF = 'firstproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'templates', 'bootstrap')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
